9666d82d63ae08060d3c19b12379fdb16789bf61
Dan Manning <dmanning23@hotmail.com>
2018-02-02 21:38:19 -0600

    moved to windx

--- TetrisRandomizer/TetrisRandomizer.WindowsDX/Properties/AssemblyInfo.cs
+++ TetrisRandomizer/TetrisRandomizer.WindowsDX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TetrisRandomizer.DesktopGL")]
+[assembly: AssemblyTitle("TetrisRandomizer.WindowsDX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TetrisRandomizer.DesktopGL")]
+[assembly: AssemblyProduct("TetrisRandomizer.WindowsDX")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
